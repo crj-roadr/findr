c62b64b4afa8610d27511b28c9fd0903008231ec
Celso Ribeiro <celsor23@gmail.com>
2026-01-18 19:28:29 +0000

    refactor: /server/index.js, server/.env.example, and /client/.env.example

--- server/index.js
+++ server/index.js
@@ -19,6 +19,11 @@ app.get('/health', (req, res) => {
   res.status(200).json({ status: 'ok' });
 });
 
+// Root
+app.get('/', (req, res) => {
+  res.status(200).json({ status: 'Findr API is running!' });
+});
+
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
